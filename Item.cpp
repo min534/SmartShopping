7d08b2b8b30998f80e880e29ae3ce43ecbe5a370
min534 <my0_530@hufs.ac.kr>
2024-12-13 22:17:50 +0900

    Update Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,11 +1,11 @@
 #include "Item.h"
 
 Item::Item(string name, double price) : name(name), price(price) {}
-
+//상품의 정보 출력
 void Item::displayInfo() const {
     cout << "상품명: " << name << ", 가격: " << price << "원" << endl;
 }
-
+// 상품의 가격 반환
 double Item::getPrice() const {
     return price;
 }
